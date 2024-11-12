dc10c0cbd28e282fc5c469d92331c472b7793c15
armedHeistKino <urban.manul.1919@gmail.com>
2024-11-13 01:02:28 +0900

    feat: Graphical은 생성시 ShaderProgram을 요구합니다.
    (1) OpenGL 기능을 래핑하는 클래스 Graphical은 이제 생성 과정에서
    ShaderProgram이 있어야 합니다.
    ShaderProgram은 섀이더와 상호작용하기 위해 반드시 필요한 클래스로써,
    OpenGL 기능이 어느정도 섀이더 프로그램과 연관성을 가지기 때문에, 해당
    기능의 추가가 필연적입니다.

--- src/core/graphical.cpp
+++ src/core/graphical.cpp
@@ -2,18 +2,19 @@
 #include <GLM/ext.hpp>
 
 #include "./application_window.cpp"
+#include "./shader_program.cpp"
 #include "../interface/IEnginable.cpp"
 #include "../auxil/exception.cpp"
 
 namespace minig {
     class Graphical : public IEnginable {
-        GLuint shader_program_name_;
+        minig::ShaderProgram shader_program_;
         GLuint attribute_array_, vertex_buffer_, index_buffer_;
     
     public:
-        Graphical() {
-            shader_program_name_ = set_program();
-        }
+        Graphical(
+            minig::ShaderProgram& shader_program
+        ): shader_program_(shader_program) {}
 
         void run() override {
             glViewport(0, 0, 800, 600);
@@ -28,22 +29,14 @@ namespace minig {
 
             float aspect_ratio = static_cast<float>(800) / static_cast<float>(600);
             glm::mat4 projection = glm::perspective(glm::radians(45.0f), aspect_ratio, 0.1f, 100.0f);
-            glUniformMatrix4fv(
-                glGetUniformLocation(shader_program_name_, "projection"), 
-                1, GL_FALSE, glm::value_ptr(projection)
-            );
+            
+            shader_program_.use();
 
-            glUniformMatrix4fv(
-                glGetUniformLocation(shader_program_name_, "view"),
-                1, GL_FALSE, glm::value_ptr(view)
-            ); 
+            shader_program_.send_float_mat4_data(std::string("projection"), projection);
+            shader_program_.send_float_mat4_data(std::string("view"), view);
+            shader_program_.send_float_mat4_data(std::string("model"), model);
 
-            glUniformMatrix4fv(
-                glGetUniformLocation(shader_program_name_, "model"),
-                1, GL_FALSE, glm::value_ptr(model)
-            ); 
 
-            glUseProgram(shader_program_name_);
             glBindVertexArray(attribute_array_);
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -82,72 +75,6 @@ namespace minig {
 
             glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(3*sizeof(GLfloat)));
             glEnableVertexAttribArray(1);
-        }
-
-        GLuint set_program() {
-            int success;
-            char log[512];
-            GLuint shader_program = glCreateProgram();
-
-            std::string vertex_source = "#version 430 core\n"
-            "layout(location = 0) in vec3 position;\n"
-            "layout(location = 1) in vec4 color;\n"
-            "uniform mat4 projection;\n"
-            "uniform mat4 view;\n"
-            "uniform mat4 model;\n"
-            "out vec4 mid_color;\n"
-            "void main() {\n"
-            "    gl_Position = projection * view * model * vec4(position, 1.0);\n"
-            "    mid_color = color;\n"
-            "}\n";
-
-            std::string fragment_source = "#version 430 core\n"
-            "in vec4 mid_color;\n"
-            "out vec4 fragment_color;\n"
-            "void main() {\n"
-            "    fragment_color = mid_color;\n"
-            "}\n"; 
-            
-            GLuint vertex = set_shader(vertex_source, GL_VERTEX_SHADER);
-            GLuint fragment = set_shader(fragment_source, GL_FRAGMENT_SHADER);
-
-            glAttachShader(shader_program, vertex);
-            glAttachShader(shader_program, fragment);
-            glLinkProgram(shader_program);
-
-            glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-            if (!success) {
-                glGetProgramInfoLog(shader_program, 512, NULL, log);
-                throw minig::auxil::ShaderProgramLinkFailure(std::string(log));
-            }
-
-            glDeleteShader(vertex);
-            glDeleteShader(fragment);
-
-            return shader_program;
-        }
-
-        GLuint set_shader(std::string& source, GLuint type) {
-            std::string shader_name;
-            if (type == GL_VERTEX_SHADER) shader_name = std::string("vertex");
-            else if (type == GL_FRAGMENT_SHADER) shader_name = std::string("fragment");
-
-            int success;
-            char log[512];
-            GLuint name = glCreateShader(type);
-
-            const char* raw_source = source.c_str();
-            glShaderSource(name, 1, &raw_source, NULL);
-            glCompileShader(name);
-            
-            glGetShaderiv(name, GL_COMPILE_STATUS, &success);
-            if (!success)
-            {
-                glGetShaderInfoLog(name, 512, NULL, log);
-                throw minig::auxil::ShaderCompilationFailure(shader_name, std::string(log)); 
-            }
-
-            return name;
         }
     };
 }
