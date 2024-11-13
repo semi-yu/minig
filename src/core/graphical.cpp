6ad0f646b96ddf2c5cf40dbc0840d75208cda413
armedHeistKino <urban.manul.1919@gmail.com>
2024-11-13 12:15:18 +0900

    feat: 여러 Shape에 대한 draw_call 호출하는 로직으로 수정

--- src/core/graphical.cpp
+++ src/core/graphical.cpp
@@ -3,13 +3,17 @@
 
 #include "./application_window.cpp"
 #include "./shader_program.cpp"
+#include "./colored_shape.cpp"
 #include "../interface/IEnginable.cpp"
 #include "../auxil/exception.cpp"
+
 
 namespace minig {
     class Graphical : public IEnginable {
         minig::ShaderProgram shader_program_;
         GLuint attribute_array_, vertex_buffer_, index_buffer_;
+        
+        std::vector<minig::ColoredShape> drawables;
     
     public:
         Graphical(
@@ -18,10 +22,14 @@ namespace minig {
 
         void run() override {
             glViewport(0, 0, 800, 600);
-            draw();
+
+            for (auto& drawable: drawables) {
+                draw_preprocess();
+                drawable.draw_call(shader_program_);
+            }
         }
 
-        void draw() {
+        void draw_preprocess() {
             glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
                              glm::vec3(0.0f, 0.0f, 0.0f),
                              glm::vec3(0.0f, 1.0f, 0.0f)); 
@@ -34,11 +42,6 @@ namespace minig {
             shader_program_.send_float_mat4_data(std::string("projection"), projection);
             shader_program_.send_float_mat4_data(std::string("view"), view);
             shader_program_.send_float_mat4_data(std::string("model"), model);
-
-            glBindVertexArray(attribute_array_);
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-            glBindVertexArray(0);
         }
 
         void configure_drawable() {
@@ -56,23 +59,12 @@ namespace minig {
                 1, 2, 3
             }); 
 
-            glGenVertexArrays(1, &attribute_array_);
-            glGenBuffers(1, &vertex_buffer_);
-            glGenBuffers(1, &index_buffer_);
+            auto triangle = minig::ColoredShape(
+                vertices,
+                indices
+            ); 
 
-            glBindVertexArray(attribute_array_);
-
-            glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
-
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
-
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)0);
-            glEnableVertexAttribArray(0);
-
-            glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(3*sizeof(GLfloat)));
-            glEnableVertexAttribArray(1);
+            drawables.push_back(triangle);
         }
     };
 }
