31cc5c593d0d1b00a3b56ced90321b34d6636ab1
armedHeistKino <urban.manul.1919@gmail.com>
2024-11-13 12:16:38 +0900

    feat: ColoredShape 클래스 추가
    (1) 개요
    ColoredShape 클래스는 OpenGL의 드로우 콜 래퍼면서, 드로우 콜을
    수행하는데 필요한 기반 작업을 묶은 클래스입니다. 클래스 하나는 화면에
    표출하고자 하는 도형 하나와 대응합니다.
    
    (2) 상세
    ColoredShape는 OpenGL를 사용하여 도형을 그리기 위한 절차를 모두
    수행합니다. VAO를 생성하고, 바인드 한 다음 VBO와 EBO를 생성하여 필요한
    vertex 및 index 데이터를 모두 GPU로 전송합니다.
    
    ColoredShape는 반드시 드로잉을 index로 하도록 합니다. 또한,
    ColoredShape는 색상만 정의된 도형을 그리는데 적합합니다. 좀 더 복잡한
    드로우 콜은 TexturedShape (WIP)을 참고합니다.
    
    ColoredShape는 다음과 같은 형태로 데이터를 전송합니다.
    
    // 위치			  // 색상(r,g,b,alpha)
    [	x,	y,	z,	r,	g,	b,	a]]

---
+++ src/core/colored_shape.cpp
@@ -0,0 +1,79 @@
+#ifndef __COLORED_SHAPE_CPP__
+#define __COLORED_SHAPE_CPP__
+
+#include <vector>
+
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <GLAD/glad.h>
+
+#include "./shader_program.cpp"
+
+namespace minig {
+    class ColoredShape {
+        GLuint attribute_array_, vertex_buffer_, index_buffer_;
+        GLuint n_indices_;
+    
+    public:
+        ColoredShape(
+            std::vector<GLfloat>& vertices,
+            std::vector<GLuint>& indices
+        ) {
+            __configure_drawable_context(vertices, indices); 
+            fmt::print(bg(fmt::color::green), "[info] {}\n", "Successful shape init.");
+        }
+
+        void draw_call(minig::ShaderProgram& shader_program) {
+            bind();
+            glDrawElements(GL_TRIANGLES, n_indices_, GL_UNSIGNED_INT, 0);
+            unbind();
+        }
+
+        void bind() { glBindVertexArray(attribute_array_); }
+
+        void unbind() { glBindVertexArray(0); }
+    
+    protected:
+
+        void __configure_drawable_context(
+            std::vector<GLfloat>& vertices,
+            std::vector<GLuint>& indices
+        ) {
+            glGenVertexArrays(1, &attribute_array_);
+            
+            bind();
+
+            __configure_vertex_buffer(vertices);
+            __configure_index_buffer(indices);
+            __configure_encoding();
+
+            unbind();
+        }
+
+        void __configure_vertex_buffer(std::vector<GLfloat>& vertices) {
+            glGenBuffers(1, &vertex_buffer_);
+
+            glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+        }
+
+        void __configure_index_buffer(std::vector<GLuint>& indices) {
+            glGenBuffers(1, &index_buffer_);
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+            n_indices_ = indices.size();
+        }
+
+        void __configure_encoding() {
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)0);
+            glEnableVertexAttribArray(0);
+
+            glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(3*sizeof(GLfloat)));
+            glEnableVertexAttribArray(1);
+        }
+    }; 
+}
+
+#endif
