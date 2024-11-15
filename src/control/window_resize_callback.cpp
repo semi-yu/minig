431882f14de272601063844b07ba9ac14b82a83f
armedHeistKino <urban.manul.1919@gmail.com>
2024-11-15 16:33:56 +0900

    feat: 윈도우 리사이즈 콜백 단순화

---
+++ src/control/window_resize_callback.cpp
@@ -0,0 +1,20 @@
+#ifndef _APPLICATION_WINDOw_RESIZE_CALLBACK_CPP__
+#define _APPLICATION_WINDOw_RESIZE_CALLBACK_CPP__
+
+#include <fmt/core.h>
+
+#include <vector>
+#include <memory>
+
+#include <GLFW/glfw3.h>
+
+namespace minig {
+    class WindowResizeCallback {
+    public:
+        void callback(GLFWwindow* window, int width, int height) {
+            glViewport(0, 0, width, height);
+        }
+    };
+}
+
+#endif
