d4f01e8475b3277a0ec9e1a0e17a9f90fdd4e57b
armedHeistKino <urban.manul.1919@gmail.com>
2024-11-17 16:42:35 +0900

    feat: 콜백 등록 필수화 및 등록 확인

--- src/control/event_handler.cpp
+++ src/control/event_handler.cpp
@@ -12,74 +12,67 @@
 
 namespace minig {
     class EventHandler {
-        std::unique_ptr<WindowResizeCallback> window_resize_callback;
-        std::unique_ptr<MouseButtonCallback> mouse_button_callback;
-        std::unique_ptr<MouseMoveCallback> mouse_move_callback;
-        std::unique_ptr<KeyboardButtonCallback> keyboard_button_callback;
+        std::unique_ptr<WindowResizeCallback> window_resize_callback_;
+        std::unique_ptr<MouseButtonCallback> mouse_button_callback_;
+        std::unique_ptr<MouseMoveCallback> mouse_move_callback_;
+        std::unique_ptr<KeyboardButtonCallback> keyboard_button_callback_;
 
     public:
-        EventHandler() {
-            window_resize_callback = std::make_unique<WindowResizeCallback>();
-            mouse_button_callback = std::make_unique<MouseButtonCallback>();
-            mouse_move_callback = std::make_unique<MouseMoveCallback>();
-            keyboard_button_callback = std::make_unique<KeyboardButtonCallback>();
-        }
-
         void register_window_resize_callback(std::unique_ptr<WindowResizeCallback> callback) {
-            window_resize_callback = std::move(callback);
+            window_resize_callback_ = std::move(callback);
         }
 
         void register_mouse_button_callback(std::unique_ptr<MouseButtonCallback> callback) {
-            mouse_button_callback = std::move(callback);
+            mouse_button_callback_ = std::move(callback);
         }
 
         void register_mouse_move_callback(std::unique_ptr<MouseMoveCallback> callback) {
-            mouse_move_callback = std::move(callback);
+            mouse_move_callback_ = std::move(callback);
         }
 
         void register_keyboard_button_callback(std::unique_ptr<KeyboardButtonCallback> callback) {
-            keyboard_button_callback = std::move(callback);
+            keyboard_button_callback_ = std::move(callback);
         }
 
         void initialize(GLFWwindow* window) {
             glfwSetWindowUserPointer(window, this);
 
-            glfwSetFramebufferSizeCallback(window, window_resize_wrapper);
-            glfwSetCursorPosCallback(window, mouse_move_wrapper);
-            glfwSetMouseButtonCallback(window, mouse_button_wrapper);
-            glfwSetKeyCallback(window, keyboard_button_wrapper);
+            if (window_resize_callback_ != nullptr) glfwSetFramebufferSizeCallback(window, window_resize_wrapper);
+            if (mouse_move_callback_ != nullptr) glfwSetCursorPosCallback(window, mouse_move_wrapper);
+            if (mouse_button_callback_ != nullptr) glfwSetMouseButtonCallback(window, mouse_button_wrapper);
+            if (keyboard_button_callback_ != nullptr) glfwSetKeyCallback(window, keyboard_button_wrapper);
         }
     
     private:
         static void window_resize_wrapper(GLFWwindow* window, int width, int height) {
             auto instance = static_cast<EventHandler*>(glfwGetWindowUserPointer(window));
 
-            if (instance) {
-                instance->window_resize_callback->callback(window, width, height);
+            if (instance && instance->window_resize_callback_ != nullptr) {
+                instance->window_resize_callback_->callback(window, width, height);
             }
         }
 
         static void mouse_move_wrapper(GLFWwindow* window, double xpos, double ypos) {
             auto instance = static_cast<EventHandler*>(glfwGetWindowUserPointer(window));
 
-            if (instance) {
-                instance->mouse_move_callback->callback(window, xpos, ypos);
+            if (instance && instance->mouse_move_callback_ != nullptr) {
+                instance->mouse_move_callback_->callback(window, xpos, ypos);
             }
         }
 
         static void mouse_button_wrapper(GLFWwindow* window, int button, int action, int mods) {
             auto instance = static_cast<EventHandler*>(glfwGetWindowUserPointer(window));
 
-            if (instance) {
-                instance->mouse_button_callback->callback(window, button, action, mods);
+            if (instance && instance->mouse_button_callback_ != nullptr) {
+                instance->mouse_button_callback_->callback(window, button, action, mods);
             }
         }
 
         static void keyboard_button_wrapper(GLFWwindow* window, int key, int scancode, int action, int mods) {
             auto instance = static_cast<EventHandler*>(glfwGetWindowUserPointer(window));
 
-            if (instance) {
-                instance->keyboard_button_callback->callback(window, key, scancode, action, mods);
+            if (instance && instance->keyboard_button_callback_ != nullptr) {
+                instance->keyboard_button_callback_->callback(window, key, scancode, action, mods);
             }
         }
     };
