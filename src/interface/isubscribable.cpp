24d2b4271b97453f879fb48552347d62ec9c7850
armedHeistKino <urban.manul.1919@gmail.com>
2024-11-17 18:15:10 +0900

    feat: ISubscribable 클래스 추가
    (1) 개요
    특정 이벤트가 발생시 invoke되도록 함. 이 클래스를 상속하여 구현한
    클래스만 이벤트 listen이 가능한 자격을 얻음.
    
    InputEvent와 마찬가지로 KeyboardEvent에 종속적. 향후 작업을 통해 추상성
    획득예정 (TBD)

---
+++ src/interface/isubscribable.cpp
@@ -0,0 +1,15 @@
+#ifndef __ISUBSCRIBABLE_CPP__
+#define __ISUBSCRIBABLE_CPP__
+
+#include <memory>
+
+#include "./input_event.cpp"
+
+namespace minig {
+    class ISubscribable {
+    public:
+        virtual void notice(std::unique_ptr<InputEvent> event) = 0;
+    };
+}
+
+#endif
