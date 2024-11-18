0217374173d52b5b0561691ca1da96758e5a1b76
armedHeistKino <urban.manul.1919@gmail.com>
2024-11-18 15:40:04 +0900

    feat: 마우스 움직임 이벤트 추가

--- src/interface/input_event.cpp
+++ src/interface/input_event.cpp
@@ -2,11 +2,31 @@
 #define __INPUT_EVENT_CPP__
 
 namespace minig {
-    class InputEvent {
+    class InputEvent {};
+
+    /*
+    */
+    class KeyboardInputEvent : public InputEvent {
     public:
         virtual int button() = 0;
-        
         virtual int action() = 0;
+        // virtual int modifier() = 0;
+        // virtual int scancode() = 0;
+    };
+
+    class MouseMoveInputEvent : public InputEvent {
+        double position_x_;
+        double position_y_;
+
+    public:
+        MouseMoveInputEvent(
+            double position_x,
+            double position_y
+        ): position_x_(position_x), position_y_(position_y) {}
+
+        double position_x() { return position_x_; }
+
+        double position_y() { return position_y_; }
     };
 }
 
